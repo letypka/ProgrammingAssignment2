973554ddb04b66839c91b9d9ee6245e901e5d6e0
Justyna Zwolak <Justyna@Justynas-MacBook-Pro.local>
2014-12-21 09:42:37 -0500

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,27 @@
 #Cached matrix will be stored in veriable "inv.matrix"
+#-----
+#Note: When working with the code, assign
+      #m1 <- matrix(c(4,2,7,5),2,2)
+      #x<-makeCacheMatrix(m1) 
+#or
+      #x<-makeCacheMatrix(matrix(c(4,2,7,5),2,2)
+#in order to use cacheSolve().
+#-----
 makeCacheMatrix <- function(x = matrix()) {
-      inv.matrix <- NULL #setting NULL for an inverse matrix
+      inv.matrix <- NULL #setting NULL if there is no inverse matrix
+      #defining the "set" function
       set <- function(y) {
             x <<- y
             inv.matrix <<- NULL
       }
+      #defining the "get" function
       get <- function() {
-            x
+            x  #get the matrix
       }
+      #defining the "set.inverse" and "get.inverse" functios
       set.inverse <- function(inv) inv.matrix <<- inv
       get.inverse <- function() inv.matrix
-#a list of functions related to a matrix that I will pass      
+      #a list of functions related to a matrix that I will pass      
       list(set = set, get = get,
            set.inverse = set.inverse,
            get.inverse = get.inverse)
@@ -19,12 +30,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
       inv.matrix <- x$get.inverse() #checking the value of "inv.matrix"
-      if(!is.null(inv.matrix)) {
-            message("getting cached data")
-            return(inv.matrix)
+      if(!is.null(inv.matrix)) { #if there is an inverse matrix already
+            message("getting cached data") #cache it
+            return(inv.matrix) #and return
       }
-      data <- x$get()
-      inv.matrix <- x$set.inverse(solve(data))
-      inv.matrix
-      ## Return a matrix that is the inverse of 'x'
+      data <- x$get() #othervise get the matrix
+      inv.matrix <- x$set.inverse(solve(data)) #calculate it's inverse
+      inv.matrix #and return it
 }
